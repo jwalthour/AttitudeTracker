96ea8fc2f90e912c851c0d2143b22a4984136516
John Walthour <john.walthour@gmail.com>
2017-01-02 22:09:57 -0500

    Added a basic test of a yaw tracker

--- src/AttitudeTracker/AttitudeTrackerTests.java
+++ src/AttitudeTracker/AttitudeTrackerTests.java
@@ -48,7 +48,7 @@ import org.jfree.data.xy.XYSeriesCollection;
 
 public class AttitudeTrackerTests {
 	public static final int NUM_DATAPOINTS = 10000;
-	public static final double DT = 0.2; // seconds
+	public static final double DT = 0.1; // seconds
 	public static final double PROCESS_NOISE_MAGNITUDE = 10;
 	public static final double X_MEASUREMENT_NOISE_MAGNITUDE = 2;
 	public static final double V_MEASUREMENT_NOISE_MAGNITUDE = 0.000001;
@@ -56,10 +56,11 @@ public class AttitudeTrackerTests {
 	public static final double PROCESS_RATE = 1-10.0/NUM_DATAPOINTS; // the only parameter of this fictional process
 
 	public static void main(String[] args) {
-		testSimpleKfWithSyntheticData(false);
-		testSpeedKfWithSyntheticData(false);
-		testAccelKfWithSyntheticData(false);
-		testUnmeasuredAccelKfWithSyntheticData(true);
+//		testSimpleKfWithSyntheticData(false);
+//		testSpeedKfWithSyntheticData(false);
+//		testAccelKfWithSyntheticData(false);
+//		testUnmeasuredAccelKfWithSyntheticData(true);
+		testTankSteerWithoutControl(true);
 	}
 	
 	/**
@@ -631,8 +632,184 @@ public class AttitudeTrackerTests {
 		showChart(chart, terminateAfter);
 	}
 
+	/**
+	 * Simulate a tank-drive robot
+	 * 
+	 */
+	public static void testTankSteerWithoutControl(boolean terminateAfter) {
+		// Generate simulated sensor data for a robot that:
+		/* - Sits still for 1s
+		 * - Pivots right for 2s
+		 * - Drives straight for 1s
+		 * - Curves left for 3s
+		 */
+		// Shadow some of the default values
+		final int NUM_DATAPOINTS = (int)((1+2+1+3) / DT);
+		final double MAX_VEHICLE_PIVOT_RATE_RAD_PER_S = Math.PI;
+		final double YAW_PROCESS_NOISE_MAGNITUDE_RAD = 5 * Math.PI / 180;
+		final double YAW_RATE_PROCESS_NOISE_MAGNITUDE_RAD_S = 5 * Math.PI / 180;
+		final double YAW_MEASUREMENT_NOISE_MAGNITUDE_RAD = 15 * Math.PI / 180;
+		final double YAW_RATE_MEASUREMENT_NOISE_MAGNITUDE_RAD_PER_S = 1 * Math.PI / 180;
+
+		double[] time          = new double[NUM_DATAPOINTS];
+		// Control input
+		double[] left_wheel_speed  = new double[NUM_DATAPOINTS];
+		double[] right_wheel_speed = new double[NUM_DATAPOINTS];
+		// Position
+		double[] yaw_ideal     = new double[NUM_DATAPOINTS];
+		double[] yaw_actual    = new double[NUM_DATAPOINTS];
+		double[] yaw_measured  = new double[NUM_DATAPOINTS];
+		double[] yaw_estimated = new double[NUM_DATAPOINTS];
+		double   yaw_cum_error = 0; // cumulative error = estimated - actual
+		double   yaw_cum_pred_error = 0; // cumulative predicted error = predicted - actual
+		// Rate - d(yaw)/dt
+		double[] yaw_rate_ideal     = new double[NUM_DATAPOINTS];
+		double[] yaw_rate_actual    = new double[NUM_DATAPOINTS];
+		double[] yaw_rate_measured  = new double[NUM_DATAPOINTS];
+		double[] yaw_rate_estimated = new double[NUM_DATAPOINTS];
+		double   yaw_rate_cum_error = 0; // cumulative error = estimated - actual
+		double   yaw_rate_cum_pred_error = 0; // cumulative predicted error = predicted - actual
+
+		for(int i = 0; i < NUM_DATAPOINTS; ++i) {
+			time[i] = i * DT;
+			boolean moving = true;
+			if(time[i] < 1.0) {
+				// Sit still to start out
+				moving = false;
+				left_wheel_speed [i] = 0.0;
+				right_wheel_speed[i] = 0.0;
+			} else if(time[i] < 3.0) {
+				// Pivot right
+				left_wheel_speed [i] = +1.0;
+				right_wheel_speed[i] = -1.0;
+				
+			} else if(time[i] < 5.0) {
+				// Go straight
+				left_wheel_speed [i] = +1.0;
+				right_wheel_speed[i] = +1.0;
+			} else  {
+				// Curve left
+				left_wheel_speed [i] =  0.0;
+				right_wheel_speed[i] = +1.0;
+			}
+			
+			// Compute turning equation
+			yaw_rate_ideal[i] = MAX_VEHICLE_PIVOT_RATE_RAD_PER_S * (left_wheel_speed[i] - right_wheel_speed[i]) / 2;
+			double yaw = (i == 0)? 0 : yaw_ideal[i - 1];
+			yaw_ideal[i] = yaw + yaw_rate_ideal[i] * DT;
+			
+			// There's no wobbling when the vehicle's stationary
+			yaw_actual[i]      = yaw_ideal[i];
+			yaw_rate_actual[i] = yaw_rate_ideal[i];
+			if(moving) {
+				yaw_actual[i]      += (2 * Math.random() - 1) * YAW_PROCESS_NOISE_MAGNITUDE_RAD;
+				yaw_rate_actual[i] += (2 * Math.random() - 1) * YAW_RATE_PROCESS_NOISE_MAGNITUDE_RAD_S;
+			}
+			
+			// There's always sensor noise
+			yaw_measured[i]      = yaw_actual[i]      + (2 * Math.random() - 1) * YAW_MEASUREMENT_NOISE_MAGNITUDE_RAD;
+			yaw_rate_measured[i] = yaw_rate_actual[i] + (2 * Math.random() - 1) * YAW_RATE_MEASUREMENT_NOISE_MAGNITUDE_RAD_PER_S;
+		}
+		
+		// Configure filter
+		KalmanFilterSimple kf = new KalmanFilterSimple();
+		DenseMatrix64F F = new DenseMatrix64F(new double[][]{
+			{1.0, DT },
+			{0.0, 1.0},
+		}); // The system dynamics model, S. Levy's tutorial calls this A
+		DenseMatrix64F Q = new DenseMatrix64F(new double[][]{
+			{YAW_PROCESS_NOISE_MAGNITUDE_RAD * 0.25, 0.0},
+			{0.0, YAW_RATE_PROCESS_NOISE_MAGNITUDE_RAD_S * 0.25},
+		}); // Noise covariance, must be estimated or ignored.  S. Levy's tutorial lacks this term, but it's added to the system dynamics model each predict step
+		DenseMatrix64F H = new DenseMatrix64F(new double[][]{
+			{1.0, 0.0},
+			{0.0, 1.0},
+		}); // Maps observations to state variables - S. Levy's tutorial calls this C
+		DenseMatrix64F R = new DenseMatrix64F(new double[][]{
+			{YAW_MEASUREMENT_NOISE_MAGNITUDE_RAD * 0.5, 0.0},
+			{0.0, YAW_RATE_MEASUREMENT_NOISE_MAGNITUDE_RAD_PER_S * 0.5},
+		}); // Sensor value variance/covariance.  This is a fake estimate for now.
+		kf.configure(F, Q, H);
+		
+		// Run filter
+		DenseMatrix64F x_init = new DenseMatrix64F(new double [][]{
+			{0.0},
+			{0.0},
+		}); // Start stationary
+		DenseMatrix64F p_init = new DenseMatrix64F(new double [][]{
+			{1, 0},
+			{0, 1},
+		}); // Arbitrary at the moment
+		kf.setState(x_init, p_init);
+		
+		for(int i = 0; i < NUM_DATAPOINTS; ++i) {
+			DenseMatrix64F z = new DenseMatrix64F(new double [][]{
+				{yaw_measured[i]},
+				{yaw_rate_measured[i]},
+			});
+			kf.predict();
+			double yaw_prediction = kf.getState().data[0];
+			double yaw_rate_prediction = kf.getState().data[1];
+			kf.update(z, R);
+			yaw_estimated[i] = kf.getState().data[0];
+			yaw_rate_estimated[i] = kf.getState().data[1];
+			double yaw_err = yaw_actual[i] - yaw_estimated[i]; 
+			yaw_cum_error += yaw_err * yaw_err;
+			double yaw_pred_err = yaw_actual[i] - yaw_prediction;
+			yaw_cum_pred_error += yaw_pred_err * yaw_pred_err;
+			double yaw_rate_err = yaw_rate_actual[i] - yaw_rate_estimated[i]; 
+			yaw_rate_cum_error += yaw_rate_err * yaw_rate_err;
+			double yaw_rate_pred_err = yaw_rate_actual[i] - yaw_rate_prediction;
+			yaw_rate_cum_pred_error += yaw_rate_pred_err * yaw_rate_pred_err;
+			
+		}
+		double yaw_stddev = Math.sqrt(yaw_cum_error / NUM_DATAPOINTS); // not actually the standard deviation but a similar computation
+		double yaw_pred_stddev = Math.sqrt(yaw_cum_pred_error / NUM_DATAPOINTS); // not actually the standard deviation but a similar computation
+		double yaw_rate_stddev = Math.sqrt(yaw_rate_cum_error / NUM_DATAPOINTS); // not actually the standard deviation but a similar computation
+		double yaw_rate_pred_stddev = Math.sqrt(yaw_rate_cum_pred_error / NUM_DATAPOINTS); // not actually the standard deviation but a similar computation
+		System.out.println("Tank steer KF: Typical yaw error: " + yaw_stddev * 180 / Math.PI + "deg, typical yaw deviation from prediction: " + yaw_pred_stddev * 180 / Math.PI);
+		System.out.println("Tank steer KF: Typical yaw rate error: " + yaw_rate_stddev * 180 / Math.PI + "deg, typical yaw rate deviation from prediction: " + yaw_rate_pred_stddev * 180 / Math.PI);
+		
+		// Graph results
+		XYSeries x_i_s = new XYSeries("Model yaw");
+		XYSeries x_a_s = new XYSeries("Actual yaw");
+		XYSeries x_m_s = new XYSeries("Measured yaw");
+		XYSeries x_e_s = new XYSeries("Estimated yaw");
+		XYSeries v_i_s = new XYSeries("Model yaw rate");
+		XYSeries v_a_s = new XYSeries("Actual yaw rate");
+		XYSeries v_m_s = new XYSeries("Measured yaw rate");
+		XYSeries v_e_s = new XYSeries("Estimated yaw rate");
+		for(int i = 1; i < NUM_DATAPOINTS; ++i) {
+			x_i_s.add(time[i], yaw_ideal[i]);
+			x_a_s.add(time[i], yaw_actual[i]);
+			x_m_s.add(time[i], yaw_measured[i]);
+			x_e_s.add(time[i], yaw_estimated[i]);
+			v_i_s.add(time[i], yaw_rate_ideal[i]);
+			v_a_s.add(time[i], yaw_rate_actual[i]);
+			v_m_s.add(time[i], yaw_rate_measured[i]);
+			v_e_s.add(time[i], yaw_rate_estimated[i]);
+		}
+		XYSeriesCollection sc = new XYSeriesCollection();
+		sc.addSeries(x_e_s);
+		sc.addSeries(x_m_s);
+		sc.addSeries(x_a_s);
+		sc.addSeries(x_i_s);
+		sc.addSeries(v_e_s);
+		sc.addSeries(v_m_s);
+		sc.addSeries(v_a_s);
+		sc.addSeries(v_i_s);
+		
+		JFreeChart chart = ChartFactory.createXYLineChart("Tank steer KF", "Time (s)", "Yaw (radians) or Yaw Rate (rad/s)", sc);
+		
+		showChart(chart, terminateAfter);
+	}
 
 	
+	/**
+	 * Display a chart in a dialog window
+	 * @param chart
+	 * @param terminateAfter
+	 */
 	private static void showChart(JFreeChart chart, boolean terminateAfter) {
 		ChartPanel panel = new ChartPanel(chart);
         panel.setFillZoomRectangle(true);
